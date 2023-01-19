25bf5e6eec3f1a3b08a35608593b1346766109e0
nataliekuechler <nataliekuechler@outlook.com>
2023-01-19 11:04:50 -0600

    Scaffolded

---
+++ SNASA.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SNASA.UI.MVC.Models
+{
+    [Keyless]
+    public class ContactViewModel
+    {
+        [Required (ErrorMessage ="*Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is Required")]
+        [DataType (DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
